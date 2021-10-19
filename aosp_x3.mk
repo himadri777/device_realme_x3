924274eab585938711d5cc982d4dff0ca0db0e9a
himadri777 <91160971+himadri777@users.noreply.github.com>
2021-10-19 07:07:47 +0530

    Update aosp_x3.mk
    Custom build type official to unofficial

--- aosp_x3.mk
+++ aosp_x3.mk
@@ -27,7 +27,7 @@ TARGET_GAPPS_ARCH := arm64
 TARGET_INCLUDE_LIVE_WALLPAPERS := true
 TARGET_SUPPORTS_GOOGLE_RECORDER := true
 TARGET_FACE_UNLOCK_SUPPORTED = true
-CUSTOM_BUILD_TYPE=OFFICIAL
+CUSTOM_BUILD_TYPE=UNOFFICIAL
 $(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
 # Device identifier
